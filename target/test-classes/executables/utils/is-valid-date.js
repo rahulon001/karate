5e687f42a6d1ac62a5c7cfb3a2434f00a6c4b10c
rahul8.ranjan <rahul8.ranjan@gmail.com>
2022-01-29 22:34:35 +0530

    karate full project

--- target/test-classes/executables/utils/is-valid-date.js
+++
@@ -1,7 +0,0 @@
-function(testDate){
-//        var date_regex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9].[0-9]+$/;
-//        if (!(date_regex.test(testDate))) {
-if (!(testDate)){
-            return true;
-         }return true;
-        }
